05d680010ff0dd0b8f935e351cc9224b1ad978be
Mashiur Mir <saurav_41@yahoo.com>
2020-06-18 15:27:37 +0900

    controllers added

---
+++ FirstProject/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstProject.Controllers
+{
+    public class StudentController : Controller
+
+    {
+        IList<Student> studentList = new List<Student>{
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+                        };
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            
+            // Get the students from the database in the real application
+
+            return View(studentList);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstProject/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FirstProject.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
