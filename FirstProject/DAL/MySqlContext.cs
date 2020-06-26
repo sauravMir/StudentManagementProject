f78009269962326b106227d0092b488742111f39
sauravMir <saurav_41@yahoo.com>
2020-06-26 14:23:07 +0900

    migration done

--- FirstProject/DAL/MySqlContext.cs
+++ FirstProject/DAL/MySqlContext.cs
@@ -2,18 +2,73 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using FirstProject.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace FirstProject.DAL
 {
+
+
     public class MySqlContext : IdentityDbContext<ApplicationUser>
     {
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
         {
+
             this.Database.EnsureCreated();
         }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            int stringMaxLength = 100 /* something like 100*/;
+            // User IdentityRole and IdentityUser in case you haven't extended those classes
+            //builder.Entity<IdentityRole>(x => x.Property(m => m.Name).HasMaxLength(stringMaxLength));
+            //builder.Entity<IdentityRole>(x => x.Property(m => m.NormalizedName).HasMaxLength(stringMaxLength));
+            //builder.Entity<ApplicationUser>(x => x.Property(m => m.NormalizedUserName).HasMaxLength(stringMaxLength));
+
+            // We are using int here because of the change on the PK
+            // builder.Entity<IdentityUserLogin<int>>(x => x.HasNoKey());
+            //builder.Entity<IdentityUserLogin<int>>(x => x.Property(m => m.LoginProvider).HasMaxLength(stringMaxLength));
+            //builder.Entity<IdentityUserLogin<int>>(x => x.Property(m => m.ProviderKey).HasMaxLength(stringMaxLength));
+
+            // We are using int here because of the change on the PK
+            // builder.Entity<IdentityUserToken<int>>(x => x.HasNoKey());
+            //builder.Entity<IdentityUserToken<int>>(x => x.Property(m => m.LoginProvider).HasMaxLength(stringMaxLength));
+            //builder.Entity<IdentityUserToken<int>>(x => x.Property(m => m.Name).HasMaxLength(stringMaxLength));
+
+            // etc..
+
+
+            // builder.Entity<ApplicationUser>().Property(u => u.UserName).HasMaxLength(128);
+
+            //builder.Entity<ApplicationUser>().Property(u => u.UserName).HasMaxLength(128);
+
+            //Uncomment this to have Email length 128 too (not neccessary)
+            //modelBuilder.Entity<ApplicationUser>().Property(u => u.Email).HasMaxLength(128);
+
+            //builder.Entity<IdentityRole>().Property(r => r.Name).HasMaxLength(128);
+
+            builder.Entity<IdentityUser>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+            builder.Entity<IdentityUser>(entity => entity.Property(m => m.NormalizedEmail).HasMaxLength(85));
+            builder.Entity<IdentityUser>(entity => entity.Property(m => m.NormalizedUserName).HasMaxLength(85));
+
+            builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+            builder.Entity<IdentityRole>(entity => entity.Property(m => m.NormalizedName).HasMaxLength(85));
+
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.LoginProvider).HasMaxLength(85));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.ProviderKey).HasMaxLength(85));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
+            builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
+
+            builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.RoleId).HasMaxLength(85));
+
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.LoginProvider).HasMaxLength(85));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.Name).HasMaxLength(85));
+
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.Id).HasMaxLength(85));
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.RoleId).HasMaxLength(85));
         }
 
         public DbSet<Student> Student { get; set; }
--- FirstProject/Models/Student.cs
+++ FirstProject/Models/Student.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace FirstProject.Models
 {
     public class Student
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StudentId { get; set; }
         [Display(Name = "Student Name")]
         public string StudentName { get; set; }
         public int Age { get; set; }
 
         public int GradeId { get; set; }
-        public Grade Class { get; set; }
+        //public Grade Grade { get; set; }
     }
 }
